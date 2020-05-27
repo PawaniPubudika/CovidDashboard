795b38c5ea7a6b7e0f6472c82d93116253d9c3fc
PawaniPubudika <pubudikalwp@gmail.com>
2020-05-27 17:35:17 +0530

    3rd commit-added cards

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,23 +2,38 @@ import React from 'react';
 import Cards from './components/Cards/Cards';
 import Charts from './components/Charts/Charts';
 import Search from './components/Search/Search';
+import styles from './App.module.css';
 import { fetchdata } from "./api";
+import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends React.Component{
+    state={
+        data:{},
+    }
+
+
 
    async componentDidMount(){
-        const data= await fetchdata();
-
-        console.log(data);
+        const fetcheddata= await fetchdata();
+        this.setState({data: fetcheddata})
+        // console.log(data);
     }
 
 
 
     render(){
-        return(
-            <div>
+        const{
+            data
+        }=this.state;
 
-            </div>
+
+        return(
+        <div className={styles.container}>
+                <Cards data={data}/>
+                <Charts/>
+                <Search/>
+        </div>
+            
         )
     }
 
--- my-app/src/components/Cards/Cards.jsx
+++ my-app/src/components/Cards/Cards.jsx
@@ -1,9 +1,61 @@
 import React from 'react';
+import styles from './Cards.module.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
-const Cards=()=>{
+
+
+const Cards = ({ data }) => {
+    console.log(data.local_total_cases);
 
     return(
-        <h1>Cards</h1>
+        <div className={styles.container}>
+                <div class="card" >
+                    {/* <img src="..." class="card-img-top" alt="..."> */}
+                    <div class="card-body">
+                    <p class="card-text">Infected <br></br>
+                        {data.local_total_cases}</p>
+                     </div>
+                </div>
+
+                <div class="card" >
+                    {/* <img src="..." class="card-img-top" alt="..."> */}
+                    <div class="card-body">
+                    <p class="card-text">Recovered</p>
+                     </div>
+                </div>
+
+                <div class="card" >
+                    {/* <img src="..." class="card-img-top" alt="..."> */}
+                    <div class="card-body">
+                    <p class="card-text">Deaths</p>
+                     </div>
+                </div>
+
+                <div class="card" >
+                    {/* <img src="..." class="card-img-top" alt="..."> */}
+                    <div class="card-body">
+                    <p class="card-text">Infected</p>
+                     </div>
+                </div>
+
+                <div class="card" >
+                    {/* <img src="..." class="card-img-top" alt="..."> */}
+                    <div class="card-body">
+                    <p class="card-text">Recovered</p>
+                     </div>
+                </div>
+
+                <div class="card" >
+                    {/* <img src="..." class="card-img-top" alt="..."> */}
+                    <div class="card-body">
+                    <p class="card-text">Deaths</p>
+                     </div>
+                </div>
+
+
+
+        </div>
+            
 
     )
 }
